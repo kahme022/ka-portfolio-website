b1c955a15344a2a619af987f73078083f4f933dd
Khalid Ahmed <31967255+kahme022@users.noreply.github.com>
2022-09-24 19:58:04 -0400

    src code for portfolio page

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/kahme022/ka-portfolio-website'
+      className='link footer__link'
+    >
+      Created By Khalid Ahmed
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,91 @@
+const header = {
+  homepage: '',
+  title: '<Ka />',
+}
+
+const about = {
+  name: 'Khalid Ahmed',
+  resume: 'https://drive.google.com/file/d/1r_XHjoHJTly1ChXHgcfFMkBQdq2kSwEA/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/kahme022/',
+    github: 'https://github.com/kahme022',
+  },
+}
+
+const experiences = [
+  {
+    company: 'Redfin - Seattle, WA',
+    role:'Software Engineer Intern',
+    date: 'May 2022 - August 2022',
+    description:
+      'Modernized an existing customer-facing feature used by over 40 million active users, which included a redesign of the UI, writing front-end unit tests, and developing APIs to support the back-end functionality for those UI components',
+    stack: ['Stack: ', 'JavaScript, ', 'Java, ', 'React, ', 'Spring, ', 'PostgreSQL'],
+  },
+  {
+    company: 'Pierre Nelson Consulting - Ottawa, ON',
+    role:'Software Engineer Intern',
+    date: 'May 2021 - August 2021',
+    description:
+      'Developed an API endpoint that generates QR Codes used by Health Canada to provide Proof of Vaccination for millions of citizens',
+    stack: ['Stack: ','Python, ', 'AWS'],
+  },
+  {
+    company: 'Correctional Service Canada - Ottawa, ON',
+    role:'Software Engineer Intern',
+    date: 'September 2020 - December 2020',
+    description:
+      'Enhanced the existing web application to conform to Web Accessibility Standards',
+    stack: ['Stack: ','Python, ', 'Django'],
+  },
+  {
+    company: 'Health Canada - Ottawa,ON',
+    role:'Software Engineer Intern',
+    date: 'January 2020 - May 2020',
+    description:
+      'Designed external REST API endpoints that allow developers read-only access to the Drug Product Database (DBD), which contains product-specific information on drugs approved for use in Canada',
+    stack: ['Stack: ','Java, ', 'Spring, ', 'JUnit, ', 'Mockito, ', 'Postman'],
+  },
+  {
+    company: 'National Research Council of Canada - Ottawa, ON',
+    role:'Junior Software Developer Intern',
+    date: 'May 2018 - September 2018',
+    description:
+      'Debugged defects related to the web application',
+    stack: ['Stack: ','HTML/CSS, ', 'JavaScript, ', 'Bootstrap'],
+  },
+]
+
+const projects = [
+  {
+    name: 'Prayer Time Mobile App',
+    description:
+      'Built a cross-platform mobile application displaying and updating Islamic prayer times for local mosques',
+    stack: ['Flutter', 'Dart'],
+    sourceCode: 'https://github.com/kahme022/prayer-time-app',
+  },
+  {
+    name: 'KA Blog',
+    description:
+      'Developed a web application that allows users to make blog posts',
+    stack: ['Django', 'Python'],
+    sourceCode: 'https://github.com/kahme022/',
+  },
+]
+
+const skills = [
+  'HTML/CSS',
+  'Python',
+  'JavaScript',
+  'Java',
+  'C++',
+  'React',
+  'Django',
+  'Git/Github',
+  'AWS',
+]
+
+const contact = {
+  email: 'kahme022@uottawa.ca',
+}
+
+export { header, about, experiences, projects, skills, contact }
